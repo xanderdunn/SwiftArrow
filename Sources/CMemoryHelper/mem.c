b6365b529988cf39e49f9e4aa54647a2c47b2181
Xander Dunn <xander@xander.ai>
2020-10-23 15:06:09 -0400

    Remove unused code and debug statements

--- Sources/CMemoryHelper/mem.c
+++ Sources/CMemoryHelper/mem.c
@@ -12,31 +12,11 @@ uint64_t getProcessMemoryUsage() {
     int i;
     if (f) {
         fscanf(f, "%s", buffer);
-        /*printf("vm: %s\n", buffer);*/
         fscanf(f, "%s", buffer);
-        /*printf("resident: %s\n", buffer);*/
         uint64_t residentPages = atoi(buffer);
-        printf("pages: %" PRIu64 "\n", residentPages);
         uint64_t residentBytes = residentPages * 4096;
-        printf("bytes: %" PRIu64 "\n", residentBytes);
         fclose (f);
         return residentBytes;
-        /*fseek (f, 0, SEEK_END);*/
-        /*length = ftell (f);*/
-        /*fseek (f, 0, SEEK_SET);*/
-        /*buffer = malloc (length);*/
-        /*if (buffer)*/
-        /*{*/
-          /*printf("Reading into buffer...\n");*/
-          /*fread (buffer, 1, length, f);*/
-        /*}*/
     }
     return 0;
-
-    /*if (buffer)*/
-    /*{*/
-      /*// start to process your data / extract strings here...*/
-      /*printf("Read this string:\n"):*/
-      /*printf("%s\n", buffer);*/
-    /*}*/
 }
